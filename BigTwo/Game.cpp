38bfd6df490e3831d4ffb07ed81bc364afebd86a
KJSMYTNFBD <101309964+KJSMYTNFBD@users.noreply.github.com>
2022-04-09 23:28:24 +0800

    添加 setCardsOnField 的內容
    將場上的牌改為導入的牌

--- BigTwo/Game.cpp
+++ BigTwo/Game.cpp
@@ -25,3 +25,13 @@ Game::Game()
 {
     this->setRpt();
 }
+void Game::setCardsOnField(double card_1,double card_2,double card_3,double card_4,double card_5)
+{
+    double card[5]={card_1,card_2,card_3,card_4,card_5};
+    for(int i=0;i<5;i++)
+    {
+        cardsOnField[i]=card[i];
+    }
+    
+}
+
