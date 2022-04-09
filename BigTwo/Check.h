2763a0ee830bee4982013ad5e8de0cb7beb6afe8
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-04-09 19:44:14 +0800

    修改函式簽名錯誤

--- BigTwo/Check.h
+++ BigTwo/Check.h
@@ -7,7 +7,7 @@ class Check
 {
 public:
     bool numberAndFlower(const double) const;
-    bool numberInPairs(const double) const;
+    bool numberInPairs(const double,const double) const;
     bool numberInTriples(const double,const double,const double) const;
 };
 
