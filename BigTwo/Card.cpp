cfd1f9d80dfafb56f832f2875fce5e3209120bbd
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-04-09 20:12:12 +0800

    添加函式setPairs,setTriples
    呼叫檔案Check的函式檢查牌型是否符合Pairs、Triples規則，並決定是否回傳所選的牌

--- BigTwo/Card.cpp
+++ BigTwo/Card.cpp
@@ -25,6 +25,33 @@ void Card::setSingle(double card)
         this->single_arr[0] = 0;
     }
 }
+void Card::setPairs(double card1, double card2) {
+    double card[2] = { card1,card2 };
+    
+    if (check.numberInPairs(card1, card2)) {
+        for (int i = 0; i < 2; i++) {
+            this->pairs_arr[i] = card[i];
+        }
+    }else {
+        for (int i = 0; i < 2; i++) {
+            this->pairs_arr[i] = 0;
+        }
+    }
+}
+void Card::setTriples(double card1, double card2, double card3) {
+    double card[3] = { card1,card2,card3 };
+
+    if (check.numberInTriples(card1, card2,card3)) {
+        for (int i = 0; i < 3; i++) {
+            this->triples_arr[i] = card[i];
+        }
+    }
+    else {
+        for (int i = 0; i < 3; i++) {
+            this->triples_arr[i] = 0;
+        }
+    }
+}
 
 int Card::getNumber() const
 {
