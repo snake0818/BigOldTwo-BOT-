be198b0178982308f1ac62c314503275e0a6db56
Gelo <gelochiang2003@gmail.com>
2022-04-15 14:39:57 +0800

    增加 get1RptCard 演算法(還未測試的部分先註解)

--- BigTwo/Game.cpp
+++ BigTwo/Game.cpp
@@ -21,21 +21,26 @@ Game::Game()
     this->setRpt();
 }
 
-void Game::setCardsOnField(double card_1, double card_2, double card_3, double card_4, double card_5)
+double Game::get1RptCard(int index)
 {
-    double card[5] = {card_1, card_2, card_3, card_4, card_5};
-    for (int index = 0; index < 5; index++)
-    {
-        cardsOnField[index] = card[index];
-    }
+    return repository[index];
 }
 
-double Game::get1CardOnField(int index) const
-{
-    return cardsOnField[index];
-}
+// void Game::setCardsOnField(double card_1, double card_2, double card_3, double card_4, double card_5)
+// {
+//     double card[5] = {card_1, card_2, card_3, card_4, card_5};
+//     for (int index = 0; index < 5; index++)
+//     {
+//         cardsOnField[index] = card[index];
+//     }
+// }
 
-double *Game::getCardsOnField() const
-{
-    return cardsOnField;
-}
+// double Game::get1CardOnField(int index) const
+// {
+//     return cardsOnField[index];
+// }
+
+// double *Game::getCardsOnField() const
+// {
+//     return cardsOnField;
+// }
