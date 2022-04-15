02666fff9f2c13a11f997560f377ac8d7d12890f
Gelo <83647608+gelochiang@users.noreply.github.com>
2022-04-15 13:18:47 +0800

    刪除無用功能、增加 get 場上的牌 function

--- BigTwo/Game.h
+++ BigTwo/Game.h
@@ -3,16 +3,17 @@
 
 class Game
 {
+public:
+    Game();
+    void setCardsOnField(double, double, double, double, double);
+    void gameStart(); // 目前沒有功能
+    double get1CardOnField(int) const;
+    double *getCardsOnField() const;
+
 private:
     double repository[52];
     void setRpt();
     static double cardsOnField[5];
-public:
-    Game();
-    double getRpt(int);
-    void setCardsOnField(double,double,double,double,double);
-    void gameStart();
-    double getCardsOnField(int);
 };
 
 #endif
