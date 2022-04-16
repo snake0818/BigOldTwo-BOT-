2be72980274b3a2715babe18d7bc5b67fd4fc67d
GeloChiang <gelochiang2003@gmail.com>
2022-04-16 14:22:04 +0800

    增加 Flush  的判斷，並完成測試

--- BigTwo/Card.cpp
+++ BigTwo/Card.cpp
@@ -95,27 +95,68 @@ void Card::setTriples(double card_1, double card_2, double card_3)
     }
 }
 
-// void Card::setFlush(double card1, double card2, double card3, double card4, double card5) {
-//     double card[5] = { card1,  card2,  card3,  card4,  card5 };
-//     int Decide = 0;
-//     arrange(card, 5);
+void Card::setFlush(double card1, double card2, double card3, double card4, double card5)
+{
+    double cards[5] = {card1, card2, card3, card4, card5};
 
-//     for (int i = 1; i < 5; i++) {
-//         if (int(card[0] * 10) % 10 == int(card[i] * 10) % 10) {
-//             Decide++;
-//         }
-//     }
-//     if (Decide == 4) {
-//         for (int i = 0; i < 5; i++) {
-//             this->flush_arr[i] = card[i];
-//         }
-//     }
-//     else {
-//         for (int i = 0; i < 5; i++) {
-//             this->flush_arr[i] = 0;
-//         }
-//     }
-// }
+    bool isInRpt = true;
+    for (int i = 0; i < 5; i++)
+    {
+        if (Check().isNumberInRpt(cards[i]) == false)
+        {
+            isInRpt = false;
+            break;
+        }
+    }
+
+    bool isSame = true;
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = i+1; j < 5; j++)
+        {
+            if (cards[i] == cards[j])
+            {
+                isSame = false;
+                break;
+            }
+        }
+    }
+
+    int Decide = 0;
+    arrange(cards, 5);
+
+    int *firstFlower = new int;
+    int *secondFlower = new int;
+    setFlower(cards[0]);
+    *firstFlower = getFlower();
+
+    for (int i = 1; i < 5; i++)
+    {
+        setFlower(cards[i]);
+        *secondFlower = getFlower();
+
+        if (*firstFlower == *secondFlower)
+        {
+            Decide++;
+        }
+    }
+
+    delete firstFlower;
+    delete secondFlower;
+
+    if (Decide == 4 && isInRpt && isSame)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            this->flush_arr[i] = cards[i];
+        }
+    }
+    else
+    {
+        // 重新出牌
+    }
+}
+
 // void Card::setTiki(double card1, double card2, double card3, double card4, double card5) {
 //     double card[5] = { card1,  card2,  card3,  card4,  card5 };
 //     arrange(card, 5);
@@ -138,32 +179,37 @@ void Card::setTriples(double card_1, double card_2, double card_3)
 //     }
 // }
 
-// int Card::getNumber() const
-// {
-//     return number;
-// }
+int Card::getNumber() const
+{
+    return number;
+}
 
-// int Card::getFlower() const
-// {
-//     return flower;
-// }
-// void Card::arrange(double *card, int size) {
-//     double temp;
-//     for (int i = 1; i < size; i++) {
-//         for (int j = 0; j < i; j++) {
-//             if (card[j] > card[i]) {
-//                 temp = card[j];
-//                 card[j] = card[i];
-//                 card[i] = temp;
-//             }
-//         }
-//     }
-// }
+int Card::getFlower() const
+{
+    return flower;
+}
+
+void Card::arrange(double *card, int size)
+{
+    double temp;
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (card[j] > card[i])
+            {
+                temp = card[j];
+                card[j] = card[i];
+                card[i] = temp;
+            }
+        }
+    }
+}
 
 // Test
 void Card::print() const
 {
-    for (auto i : triples_arr)
+    for (auto i : flush_arr)
         cout << i << " ";
     cout << endl;
 }
