afce80efb4c6df80a28c5eaa89da80c3aff90e9e
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-04-29 23:46:09 +0800

    新增基礎函示

--- BigTwo/Computer.cpp
+++ BigTwo/Computer.cpp
@@ -1,21 +1,53 @@
 #include "Computer.h"
 
+void Computer::setArr(double Num, int index)
+{
+    ComputerArr[index] = Num;
+}
+
+bool Computer::find(double num)
+{
+    for (int i = 0; i < 13; i++) {
+        if (ComputerArr[i] == num) return true;
+    }
+    return false;
+}
+
+int Computer::getNumOfCards() const
+{
+    int count = 0;
+    for (int i = 0; i < 13; i++) {
+        if (ComputerArr[i] != 0) count++;
+    }
+    return count;
+}
+void Computer::arrange()
+{
+    const int size = 13;
+
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (ComputerArr[j] > ComputerArr[i])
+            {
+                double temp = ComputerArr[j];
+                ComputerArr[j] = ComputerArr[i];
+                ComputerArr[i] = temp;
+            }
+        }
+    }
+}
+
+double Computer::getCard(int index)
+{
+    return ComputerArr[index];
+}
+
 void Computer::print()
 {
     for (int i = 0; i < 13; i++) {
-        cout << setw(6) << computerArr[i];
+        cout << setw(6) << ComputerArr[i];
     }
     cout << endl;
-}
-
-Computer::Computer()
-{
-
-}
-
-Computer::Computer(double card[])
-{
-    for (int i = 0; i < 13; i++) {
-        computerArr[i] = card[i];
-    }
 }
