b1f3e99f0e0adb4efc2e862b9910c30970dedbe8
KJSMYTNFBD <101309964+KJSMYTNFBD@users.noreply.github.com>
2022-04-29 14:58:57 +0800

    補充Computer() , Computer(double[]) 的內容
    Computer(double[]) 可以直接導入array 去修改 computerArr的值

--- BigTwo/Computer.cpp
+++ BigTwo/Computer.cpp
@@ -3,7 +3,19 @@
 void Computer::print()
 {
     for (int i = 0; i < 13; i++) {
-        cout << setw(6) << ComputerArr[i];
+        cout << setw(6) << computerArr[i];
     }
     cout << endl;
+}
+
+Computer::Computer()
+{
+
+}
+
+Computer::Computer(double card[])
+{
+    for (int i = 0; i < 13; i++) {
+        computerArr[i] = card[i];
+    }
 }
