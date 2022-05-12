bf05fcf76097c3b23f465a7760b3b68e1ddf8107
GeloChiang <gelochiang2003@gmail.com>
2022-05-12 17:39:45 +0800

    已新增發牌以及分牌並完成測試

---
+++ BigTwo/main.cpp
@@ -0,0 +1,24 @@
+#include "Card.h"
+#include "Check.h"
+#include "Computer.h"
+#include "Game.h"
+#include "Player.h"
+#include "Table.h"
+#include "Tool.h"
+
+int main()
+{
+    Table play;
+    play.start();
+
+    
+    /*    
+        以下部分都通過測試
+        play.getPlayer().print();
+        play.getComputer1().print();
+        play.getComputer2().print();
+        play.getComputer3().print();
+    */
+
+    return 0;
+}
