f3043c03bf992a7269404d7109277c13aad4f92d
zeroDavid70 <101310030+zeroDavid70@users.noreply.github.com>
2022-05-30 17:53:26 +0800

    增加判斷出牌是否有在手牌內

--- BigTwo/main.cpp
+++ BigTwo/main.cpp
@@ -61,27 +61,107 @@ int main()
         cout << "請輸入英文+數字方式出牌，Ex:方塊3 = D3 or d3" << endl << endl;
 
         Computer c1;
-
         Game g;
-
         Card card1;
 
         Player p;
         p.playerOutCard();
 
-        double ca[5] = { 0 };
+        double ca[5] = {0,0,0,0,0};
 
+        int caSize = 0;
+        /**/
         for (int i = 0;i < 5;i++) {
             if (g.get1CardOnField(i) != 0) {
-                cout << g.get1CardOnField(i) << " ";
+                ca[i] = 0;
                 ca[i] = g.get1CardOnField(i);
+                caSize++;
             }
         }
-        cout << endl<<endl;
-        Check ch;
-        int Casize = sizeof(ca) / sizeof(ca[0]);
 
+        int t = 0;
+
+        switch (sequence) {
+        case 1:
+            for (int i = 0;i < 13;i++) {
+                for (int j = 0;j < 5;j++) {
+                    if (ca[j] != play.getPlayer().getIndexOfCard(i)) {
+                        t++;
+                    }
+                }
+            }
+            if ((t + caSize) % 13 != 0) {
+                cout << "please recin";
+            }
+            else {
+                for (int i = 0;i < caSize;i++) {
+                    cout << ca[i] << " ";
+                }
+            }
+            t = 0;
+            break;
+        case 2:
+            for (int i = 0;i < 13;i++) {
+                for (int j = 0;j < 5;j++) {
+                    if (ca[j] != play.getComputer1().getIndexOfCard(i)) {
+                        t++;
+                    }
+                }
+            }
+            if ((t + caSize) % 13 != 0) {
+                cout << "please recin";
+            }
+            else {
+                for (int i = 0;i < caSize;i++) {
+                    cout << ca[i] << " ";
+                }
+            }
+            t = 0;
+            break;
+        case 3:
+            for (int i = 0;i < 13;i++) {
+                for (int j = 0;j < 5;j++) {
+                    if (ca[j] != play.getComputer2().getIndexOfCard(i)) {
+                        t++;
+                    }
+                }
+            }
+            if ((t + caSize) % 13 != 0) {
+                cout << "please recin";
+            }
+            else {
+                for (int i = 0;i < caSize;i++) {
+                    cout << ca[i] << " ";
+                }
+            }
+            t = 0;
+            break;
+        case 4:
+            for (int i = 0;i < 13;i++) {
+                for (int j = 0;j < 5;j++) {
+                    if (ca[j] != play.getComputer3().getIndexOfCard(i)) {
+                        t++;
+                    }
+                }
+            }
+            if ( (t + caSize) % 13 != 0) {
+                cout << "please recin";
+            }
+            else {
+                for (int i = 0;i < caSize;i++) {
+                    cout << ca[i] << " ";
+                }
+            }
+            t = 0;
+            break;
+        }
+
+        cout << endl << endl;
+
+        /*
+        Check ch;
         cout << ch.checkCardsType(ca,Casize)<<endl;
+        */
         
         if (sequence != 4) {
             sequence++;
@@ -103,8 +183,5 @@ int main()
         cout << "現在是第" << sequence << "位出牌" << endl;
         cout << endl;
         }
-        
-
-
     return 0;
 }
