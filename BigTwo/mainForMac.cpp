ffed11de289a5effc5d7f44eafc7b2ca051f0d67
GeloChiang <gelochiang2003@gmail.com>
2022-06-01 20:40:58 +0800

    新增印出桌面的牌

--- BigTwo/mainForMac.cpp
+++ BigTwo/mainForMac.cpp
@@ -21,17 +21,19 @@ int main()
 
     print.printSequence(sequence);
 
-    // play.getComputer1().print();
-    // play.getComputer2().print();
-    // play.getComputer3().print();
+    play.getComputer1().print();
+    play.getComputer2().print();
+    play.getComputer3().print();
     
     // test
-    // play.getPlayer().printPlayer_arr();
+    play.getPlayer().printPlayer_arr();
     // OutOfCard().computerFirstOutHand(play.getComputer1());
     // OutOfCard().computerFirstOutHand(play.getComputer2());
     // OutOfCard().computerFirstOutHand(play.getComputer3());
 
     play.outCardSequence();
+
+    print.printTable();
 
     // while(sequence != 4) // while for test
     // {
