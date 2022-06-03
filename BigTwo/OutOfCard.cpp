79daad573072aa5ce01e8faf0369c78724205c92
GeloChiang <gelochiang2003@gmail.com>
2022-06-03 17:23:26 +0800

    把還未完全測試的先註解

--- BigTwo/OutOfCard.cpp
+++ BigTwo/OutOfCard.cpp
@@ -6,34 +6,34 @@
 
 OutOfCard::OutOfCard() {}
 
-void OutOfCard::computerFirstOutHand(Computer& computer)
+void OutOfCard::computerFirstOutHand(Computer &computer)
 {
     double *computerArr = computer.getComputer_arr();
-    
-    /* 
+
+    /*
         電腦要是有方塊 3 出牌想法很簡單
         先判斷能否構成 3 tiki 無法的話
         在判斷能否構成 3 fullhouse...
         依序類推 3 pairs, 3 single
     */
     int numOfThree = 0;
-    for(int i = 0; i < 13; i++)
+    for (int i = 0; i < 13; i++)
     {
         int card = computer.getIndexOfCard(i);
-        if(card == 3)
+        if (card == 3)
         {
             numOfThree++;
         }
-        if(card > 3)
+        if (card > 3)
         {
             break;
         }
     }
 
-    if(numOfThree == 4)
+    if (numOfThree == 4)
     {
         int index = computer.returnIndex(3.1);
-        for(int i = 0; i < 4; i++, index++)
+        for (int i = 0; i < 4; i++, index++)
         {
             Game().setField(computer.getIndexOfCard(index), i);
             computer.setComputerArr(0, index);
@@ -41,12 +41,12 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
 
         int singleIndex;
         bool allPairs = true;
-        for(int i = index; i < 12; i++)
+        for (int i = index; i < 12; i++)
         {
             int num1 = Card().returnNumber(computer.getIndexOfCard(i));
-            int num2 = Card().returnNumber(computer.getIndexOfCard(i+1));
+            int num2 = Card().returnNumber(computer.getIndexOfCard(i + 1));
 
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 i++;
             }
@@ -58,7 +58,7 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
             }
         }
 
-        if(allPairs)
+        if (allPairs)
         {
             Game().setField(computer.getIndexOfCard(index), 4);
             computer.setComputerArr(0, index);
@@ -73,17 +73,17 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
 
         Table().setCardsType(697);
     }
-    else if(numOfThree == 3)
+    else if (numOfThree == 3)
     {
         int index = computer.returnIndex(3.1);
         int pairsIndex;
         bool isFullHouse = false;
-        for(int i = index+3; i < 12; i++)
+        for (int i = index + 3; i < 12; i++)
         {
             int num1 = Card().returnNumber(computer.getIndexOfCard(i));
-            int num2 = Card().returnNumber(computer.getIndexOfCard(i+1));
+            int num2 = Card().returnNumber(computer.getIndexOfCard(i + 1));
 
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 pairsIndex = i;
                 isFullHouse = true;
@@ -91,15 +91,15 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
             }
         }
 
-        if(isFullHouse)
+        if (isFullHouse)
         {
-            for(int i = 0; i < 3; i++, index++)
+            for (int i = 0; i < 3; i++, index++)
             {
                 Game().setField(computer.getIndexOfCard(index), i);
                 computer.setComputerArr(0, index);
             }
 
-            for(int i = 3; i < 5; i++, pairsIndex++)
+            for (int i = 3; i < 5; i++, pairsIndex++)
             {
                 Game().setField(computer.getIndexOfCard(pairsIndex), i);
                 computer.setComputerArr(0, pairsIndex);
@@ -111,7 +111,7 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
         }
         else
         {
-            for(int i = 0; i < 3; i++, index++)
+            for (int i = 0; i < 3; i++, index++)
             {
                 Game().setField(computer.getIndexOfCard(index), i);
                 computer.setComputerArr(0, index);
@@ -122,11 +122,11 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
             Table().setCardsType(693);
         }
     }
-    else if(numOfThree == 2)
+    else if (numOfThree == 2)
     {
         int index = computer.returnIndex(3.1);
-        
-        for(int i = 0; i < 2; i++, index++)
+
+        for (int i = 0; i < 2; i++, index++)
         {
             Game().setField(computer.getIndexOfCard(index), i);
             computer.setComputerArr(0, index);
@@ -136,7 +136,7 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
 
         Table().setCardsType(692);
     }
-    else if(numOfThree == 1)
+    else if (numOfThree == 1)
     {
         int index = computer.returnIndex(3.1);
         Game().setField(3.1, 0);
@@ -152,43 +152,41 @@ void OutOfCard::computerFirstOutHand(Computer& computer)
     }
 }
 
-void OutOfCard::computerOutHand(Computer& computer)
+void OutOfCard::computerOutHand(Computer &computer)
 {
     int type = Table().getCardsType();
 
     bool isOutHand = false;
-    if(type == 691)
+    if (type == 691)
     {
-        /* 
+        /*
             場上單張時
             判斷有沒有單張可以出，有的話出單張
         */
-        if(computer.getHandCardsSize() > 4)
+        if (computer.getHandCardsSize() > 4)
         {
-
         }
-        else if(computer.getHandCardsSize() > 3)
+        else if (computer.getHandCardsSize() > 3)
         {
-
         }
-        else if(computer.getHandCardsSize() > 2)
+        else if (computer.getHandCardsSize() > 2)
         {
             const int INDEX = 10;
             double num1 = Card().returnNumber(computer.getIndexOfCard(INDEX));
             int sameOfNum = 1;
-            for(int i = INDEX+1; i < 13; i++)
+            for (int i = INDEX + 1; i < 13; i++)
             {
                 double num2 = Card().returnNumber(computer.getIndexOfCard(i));
-                if(num1 == num2)
+                if (num1 == num2)
                 {
                     sameOfNum++;
                 }
             }
 
-            if(sameOfNum == 1)
+            if (sameOfNum == 1)
             {
                 double card = computer.getIndexOfCard(10);
-                if(Compare().singleCompare(card))
+                if (Compare().singleCompare(card))
                 {
                     Game().setField(card, 0);
                     computer.setComputerArr(0, 10);
@@ -196,10 +194,10 @@ void OutOfCard::computerOutHand(Computer& computer)
                     isOutHand = true;
                 }
             }
-            else if(sameOfNum == 2)
+            else if (sameOfNum == 2)
             {
                 double lastOne = computer.getIndexOfCard(12);
-                if(Compare().singleCompare(lastOne))
+                if (Compare().singleCompare(lastOne))
                 {
                     Game().setField(lastOne, 0);
                     computer.setComputerArr(0, 12);
@@ -207,12 +205,12 @@ void OutOfCard::computerOutHand(Computer& computer)
                     isOutHand = true;
                 }
             }
-            else if(sameOfNum == 3)
+            else if (sameOfNum == 3)
             {
-                for(int i = INDEX; i < 13; i++)
+                for (int i = INDEX; i < 13; i++)
                 {
                     double card = computer.getIndexOfCard(i);
-                    if(Compare().singleCompare(card))
+                    if (Compare().singleCompare(card))
                     {
                         Game().setField(card, 0);
                         computer.setComputerArr(0, i);
@@ -223,12 +221,12 @@ void OutOfCard::computerOutHand(Computer& computer)
                 }
             }
         }
-        else if(computer.getHandCardsSize() > 1)
+        else if (computer.getHandCardsSize() > 1)
         {
-            for(int i = 11; i < 13; i++)
+            for (int i = 11; i < 13; i++)
             {
                 double card = computer.getIndexOfCard(i);
-                if(Compare().singleCompare(card))
+                if (Compare().singleCompare(card))
                 {
                     Game().setField(card, 0);
                     computer.setComputerArr(0, i);
@@ -241,7 +239,7 @@ void OutOfCard::computerOutHand(Computer& computer)
         else
         {
             double lastOne = computer.getIndexOfCard(12);
-            if(Compare().singleCompare(lastOne))
+            if (Compare().singleCompare(lastOne))
             {
                 Game().setField(lastOne, 0);
                 computer.setComputerArr(0, 12);
@@ -250,148 +248,157 @@ void OutOfCard::computerOutHand(Computer& computer)
         }
     }
     else if (type == 692)
-    {   /*  場上呸時 判斷有沒有呸可以出，有的話出呸    */
-        double card[5] = { 0 };
+    { /*  場上呸時 判斷有沒有呸可以出，有的話出呸    */
+        // double card[5] = {0};
 
-        double minValue = Game().getCardsOnField()[0];
-        for (int i = 1; i < 5; i++)
-        {
-            if (Game().getCardsOnField()[i] < minValue) minValue = Game().getCardsOnField()[i];
-        }
+        // double minValue = Game().getCardsOnField()[0];
+        // for (int i = 1; i < 5; i++)
+        // {
+        //     if (Game().getCardsOnField()[i] < minValue)
+        //         minValue = Game().getCardsOnField()[i];
+        // }
 
-        if (Check().checkPairs(Computer().getComputer_arr(), card, minValue))
-        {
-            if (Compare().pairsCompare(card))
-            {
-                Tool().arrange(card, 5);
-                for (int i = 0; i < 2; i++)
-                {
-                    Game().setField(card, 5);
-                }
-            }
-        }
-        else isOutHand = true;
+        // if (Check().checkPairs(Computer().getComputer_arr(), card, minValue))
+        // {
+        //     if (Compare().pairsCompare(card))
+        //     {
+        //         Tool().arrange(card, 5);
+        //         for (int i = 0; i < 2; i++)
+        //         {
+        //             Game().setField(card, 5);
+        //         }
+        //     }
+        // }
+        // else
+        //     isOutHand = true;
     }
     else if (type == 693)
-    {/* 場上三條時 判斷有沒有三條可以出，有的話出三條 */
-        double card[5] = { 0 };
+    { /* 場上三條時 判斷有沒有三條可以出，有的話出三條 */
+        // double card[5] = {0};
 
-        double minValue = Game().getCardsOnField()[0];
-        for (int i = 1; i < 5; i++)
-        {
-            if (Game().getCardsOnField()[i] < minValue) minValue = Game().getCardsOnField()[i];
-        }
+        // double minValue = Game().getCardsOnField()[0];
+        // for (int i = 1; i < 5; i++)
+        // {
+        //     if (Game().getCardsOnField()[i] < minValue)
+        //         minValue = Game().getCardsOnField()[i];
+        // }
 
-        if (Check().checkTriples(Computer().getComputer_arr(), card, minValue))
-        {
-            if (Compare().tripleCompare(card))
-            {
-                Tool().arrange(card, 5);
-                for (int i = 0; i < 3; i++)
-                {
-                    Game().setField(card, 5);
-                }
-            }
-        }
-        else isOutHand = true;
+        // if (Check().checkTriples(Computer().getComputer_arr(), card, minValue))
+        // {
+        //     if (Compare().tripleCompare(card))
+        //     {
+        //         Tool().arrange(card, 5);
+        //         for (int i = 0; i < 3; i++)
+        //         {
+        //             Game().setField(card, 5);
+        //         }
+        //     }
+        // }
+        // else
+        //     isOutHand = true;
     }
     else if (type == 694)
-    {/* 場上順子時 判斷有沒有順子可以出，有的話出順子 */
-        double card[5] = { 0 };
+    { /* 場上順子時 判斷有沒有順子可以出，有的話出順子 */
+        // double card[5] = {0};
 
-        double minValue = Game().getCardsOnField()[0];
-        for (int i = 1; i < 5; i++)
-        {
-            if (Game().getCardsOnField()[i] < minValue) minValue = Game().getCardsOnField()[i];
-        }
+        // double minValue = Game().getCardsOnField()[0];
+        // for (int i = 1; i < 5; i++)
+        // {
+        //     if (Game().getCardsOnField()[i] < minValue)
+        //         minValue = Game().getCardsOnField()[i];
+        // }
 
-        if (Check().checkStraight(Computer().getComputer_arr(), card, minValue))
-        {
-            if (Compare().straightCompare(card))
-            {
-                Tool().arrange(card, 5);
-                for (int i = 0; i < 3; i++)
-                {
-                    Game().setField(card, 5);
-                }
-            }
-        }
-        else isOutHand = true;
+        // if (Check().checkStraight(Computer().getComputer_arr(), card, minValue))
+        // {
+        //     if (Compare().straightCompare(card))
+        //     {
+        //         Tool().arrange(card, 5);
+        //         for (int i = 0; i < 3; i++)
+        //         {
+        //             Game().setField(card, 5);
+        //         }
+        //     }
+        // }
+        // else
+        //     isOutHand = true;
     }
     else if (type == 695)
-    {/* 場上同花時 判斷有沒有同花可以出，有的話出同花 */
-        double card[5] = { 0 };
+    { /* 場上同花時 判斷有沒有同花可以出，有的話出同花 */
+        // double card[5] = {0};
 
-        double minValue = Game().getCardsOnField()[0];
-        for (int i = 1; i < 5; i++)
-        {
-            if (Game().getCardsOnField()[i] < minValue) minValue = Game().getCardsOnField()[i];
-        }
+        // double minValue = Game().getCardsOnField()[0];
+        // for (int i = 1; i < 5; i++)
+        // {
+        //     if (Game().getCardsOnField()[i] < minValue)
+        //         minValue = Game().getCardsOnField()[i];
+        // }
 
-        if (Check().checkTriples(Computer().getComputer_arr(), card, minValue))
-        {
-            if (Compare().tripleCompare(card))
-            {
-                Tool().arrange(card, 5);
-                for (int i = 0; i < 3; i++)
-                {
-                    Game().setField(card, 5);
-                }
-            }
-        }
-        else isOutHand = true;
+        // if (Check().checkTriples(Computer().getComputer_arr(), card, minValue))
+        // {
+        //     if (Compare().tripleCompare(card))
+        //     {
+        //         Tool().arrange(card, 5);
+        //         for (int i = 0; i < 3; i++)
+        //         {
+        //             Game().setField(card, 5);
+        //         }
+        //     }
+        // }
+        // else
+        //     isOutHand = true;
     }
     else if (type == 696)
-    {/* 場上葫蘆時 判斷有沒有葫蘆可以出，有的話出葫蘆 */
-        double card[5] = { 0 };
+    { /* 場上葫蘆時 判斷有沒有葫蘆可以出，有的話出葫蘆 */
+        // double card[5] = {0};
 
-        double minValue = Game().getCardsOnField()[0];
+        // double minValue = Game().getCardsOnField()[0];
 
-        Tool().arrange(Game().getCardsOnField(), 5);
-        if (Card().returnNumber(Game().getCardsOnField()[0]) != Card().returnNumber(Game().getCardsOnField()[2]))
-            minValue = Game().getCardsOnField()[2];
+        // Tool().arrange(Game().getCardsOnField(), 5);
+        // if (Card().returnNumber(Game().getCardsOnField()[0]) != Card().returnNumber(Game().getCardsOnField()[2]))
+        //     minValue = Game().getCardsOnField()[2];
 
-        if (Check().checkFullHouse(Computer().getComputer_arr(), card, minValue))
-        {
-            if (Compare().fullHouseCompare(card))
-            {
-                Tool().arrange(card, 5);
-                for (int i = 0; i < 3; i++)
-                {
-                    Game().setField(card, 5);
-                }
-            }
-        }
-        else isOutHand = true;
+        // if (Check().checkFullHouse(Computer().getComputer_arr(), card, minValue))
+        // {
+        //     if (Compare().fullHouseCompare(card))
+        //     {
+        //         Tool().arrange(card, 5);
+        //         for (int i = 0; i < 3; i++)
+        //         {
+        //             Game().setField(card, 5);
+        //         }
+        //     }
+        // }
+        // else
+        //     isOutHand = true;
     }
 
     if (!isOutHand)
     {
         if (type == 697)
-        {/* 場上鐵支時 判斷有沒有鐵支可以出，有的話出鐵支 */
-            double card[5] = { 0 };
+        { /* 場上鐵支時 判斷有沒有鐵支可以出，有的話出鐵支 */
+            // double card[5] = {0};
 
-            double minValue = Game().getCardsOnField()[0];
-            Tool().arrange(Game().getCardsOnField(), 5);
-            if (Card().returnNumber(Game().getCardsOnField()[0]) != Card().returnNumber(Game().getCardsOnField()[1]))
-                minValue = Game().getCardsOnField()[4];
+            // double minValue = Game().getCardsOnField()[0];
+            // Tool().arrange(Game().getCardsOnField(), 5);
+            // if (Card().returnNumber(Game().getCardsOnField()[0]) != Card().returnNumber(Game().getCardsOnField()[1]))
+            //     minValue = Game().getCardsOnField()[4];
 
-            if (Check().checkTiki(Computer().getComputer_arr(), card, minValue))
-            {
-                if (Compare().tikiCompare(card))
-                {
-                    Tool().arrange(card, 5);
-                    for (int i = 0; i < 3; i++)
-                    {
-                        Game().setField(card, 5);
-                    }
-                }
-            }
-            else isOutHand = true;
+            // if (Check().checkTiki(Computer().getComputer_arr(), card, minValue))
+            // {
+            //     if (Compare().tikiCompare(card))
+            //     {
+            //         Tool().arrange(card, 5);
+            //         for (int i = 0; i < 3; i++)
+            //         {
+            //             Game().setField(card, 5);
+            //         }
+            //     }
+            // }
+            // else
+            //     isOutHand = true;
         }
         if (!isOutHand && type == 698)
-        {/* 場上同花順時 判斷有沒有同花順可以出，有的話出同花順 */
-            
+        { /* 場上同花順時 判斷有沒有同花順可以出，有的話出同花順 */
         }
     }
 
@@ -402,14 +409,7 @@ void OutOfCard::computerOutHand(Computer& computer)
     }
 }
 
-    if(!isOutHand)
-    {
-        // Computer pass
-        cout << "Cpmputer pass" << endl;
-    }
-}
-
-void OutOfCard::computer_2outHand(Computer& computer)
+void OutOfCard::computer_2outHand(Computer &computer)
 {
     // wait for programing.
 }
