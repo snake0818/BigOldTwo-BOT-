442e07f689502360c44d10cd1f26db25d2e8b0a2
GeloChiang <gelochiang2003@gmail.com>
2022-06-03 14:23:19 +0800

    更改測試格式

--- BigTwo/mainForMac.cpp
+++ BigTwo/mainForMac.cpp
@@ -47,22 +47,22 @@ int main()
     play.getComputer2().setComputerArr(0, 8);
     play.getComputer2().setComputerArr(0, 9);
     play.getComputer2().setComputerArr(0, 10);
-    play.getComputer2().setComputerArr(0, 11);
+    play.getComputer2().setComputerArr(1.1, 11);
     play.getComputer2().setComputerArr(2.1, 12);
 
-    play.getComputer3().setComputerArr(1.1, 0);
-    play.getComputer3().setComputerArr(3.1, 1);
-    play.getComputer3().setComputerArr(3.2, 2);
-    play.getComputer3().setComputerArr(3.3, 3);
-    play.getComputer3().setComputerArr(3.4, 4);
-    play.getComputer3().setComputerArr(4.1, 5);
-    play.getComputer3().setComputerArr(4.2, 6);
-    play.getComputer3().setComputerArr(5.1, 7);
-    play.getComputer3().setComputerArr(5.2, 8);
-    play.getComputer3().setComputerArr(6.1, 9);
-    play.getComputer3().setComputerArr(6.2, 10);
-    play.getComputer3().setComputerArr(7.1, 11);
-    play.getComputer3().setComputerArr(8.1, 12);
+    play.getComputer3().setComputerArr(0, 0);
+    play.getComputer3().setComputerArr(0, 1);
+    play.getComputer3().setComputerArr(0, 2);
+    play.getComputer3().setComputerArr(0, 3);
+    play.getComputer3().setComputerArr(0, 4);
+    play.getComputer3().setComputerArr(0, 5);
+    play.getComputer3().setComputerArr(0, 6);
+    play.getComputer3().setComputerArr(0, 7);
+    play.getComputer3().setComputerArr(0, 8);
+    play.getComputer3().setComputerArr(0, 9);
+    play.getComputer3().setComputerArr(0, 10);
+    play.getComputer3().setComputerArr(1.1, 11);
+    play.getComputer3().setComputerArr(2.1, 12);
 
     // test
     play.getComputer1().print();
@@ -72,7 +72,7 @@ int main()
 
     play.outCardSequence();
 
-    Print().printTable();
+    // Print().printTable();
 
     // test
     play.getComputer1().print();
