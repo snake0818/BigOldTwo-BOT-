a28739d92bb65284a0de5d48cd1419bc87542348
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-06-03 09:08:26 +0800

    將排序做修改，新增Print方便做測試

--- BigTwo/Tool.cpp
+++ BigTwo/Tool.cpp
@@ -17,6 +17,14 @@ void Tool::arrange(double *card, int size)
             }
         }
     }
+    while (card[0] == 0)
+    {
+        for (int i = 1; i < size; i++)
+        {
+            card[i - 1] = card[i];
+        }
+        card[4] = 0;
+    }
 }
 
 void Tool::copyDouble_arr(double sourceArr[], double targetArr[], int size)
@@ -25,4 +33,13 @@ void Tool::copyDouble_arr(double sourceArr[], double targetArr[], int size)
     {
         targetArr[i] = sourceArr[i];
     }
+}
+
+void Tool::Print(double* cards, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << setw(6) << cards[i];
+    }
+    cout << endl;
 }
