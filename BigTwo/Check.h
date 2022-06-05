49556607087f3ff8647f53ef3a2955473b13f8b3
GeloChiang <gelochiang2003@gmail.com>
2022-06-05 20:27:10 +0800

    回到先前版本，新加的函式出現錯誤

--- BigTwo/Check.h
+++ BigTwo/Check.h
@@ -1,30 +1,16 @@
-#ifndef CHECK_H
-#define CHECH_H
+#include "Game.h"
+#include "Table.h"
 
+#ifndef CHECK_H
+#define CHECK_H
 class Check
 {
 public:
-	bool isNumberInRpt(const double card) const;
-	bool checkInRpt(double* cards);
-	bool checkSame(double* cards);
-	bool checkPairs(double* cards);
-	bool checkTriples(double* cards);
-	bool checkFlush(double* cards);
-	bool checkStraight(double* cards);
-	bool checkTiki(double* cards);
-	bool checkStraightFlush(double* cards);
-	bool checkSpecialStraight(double* cards);
-	bool checkFullHouse(double* cards);
-	int checkCardsType(double* cards, double* card);
-	int checkCardsType(double* cards, int num);
-	bool checkSingle(double* cards, double* card, double value);
-	bool checkPairs(double* cards, double* card, double value);
-	bool checkTriples(double* cards, double* card, double value);
-	bool checkFlush(double* cards, double* card, double value);
-	bool checkStraight(double* cards, double* card, double value);
-	bool checkFullHouse(double* cards, double* card, double value);
-	bool checkTiki(double* cards, double* card, double value);
-	bool checkStraightFlush(double* cards, double* card, double value);
+	bool isNumberInRpt(const double) const;
+	void checkInRpt(double[], bool &);
+	void checkSame(double[], bool &);
+	void checkFlush(double[], bool &);
+	void checkStraight(double[], bool &);
+	void checkSpecialStraight(double[], bool &);
 };
-
 #endif
