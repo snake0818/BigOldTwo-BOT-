04bd0a6b77280bfcf49b7302e79e5135fd82e827
GeloChiang <gelochiang2003@gmail.com>
2022-06-07 19:54:02 +0800

    更改測試格式

--- BigTwo/mainForMac.cpp
+++ BigTwo/mainForMac.cpp
@@ -21,12 +21,19 @@ int main()
 
     Print().printSequence(sequence);
 
+    Game().setField(3.1, 0);
+    Game().setField(3.3, 1);
+    Game().setField(3.4, 2);
+    Game().setField(0, 3);
+    Game().setField(0, 4);
+    Table().setCardsType(693);
+
     // test
-    // play.getComputer1().setComputerArr(1.1, 0);
-    // play.getComputer1().setComputerArr(3.1, 1);
-    // play.getComputer1().setComputerArr(3.4, 2);
-    // play.getComputer1().setComputerArr(5.3, 3);
-    // play.getComputer1().setComputerArr(6.4, 4);
+    // play.getComputer1().setComputerArr(1.3, 0);
+    // play.getComputer1().setComputerArr(2.4, 1);
+    // play.getComputer1().setComputerArr(5.3, 2);
+    // play.getComputer1().setComputerArr(6.1, 3);
+    // play.getComputer1().setComputerArr(7.1, 4);
     // play.getComputer1().setComputerArr(4.1, 5);
     // play.getComputer1().setComputerArr(4.2, 6);
     // play.getComputer1().setComputerArr(5.1, 7);
@@ -63,6 +70,13 @@ int main()
     // play.getComputer3().setComputerArr(6.2, 10);
     // play.getComputer3().setComputerArr(3.2, 11);
     // play.getComputer3().setComputerArr(3.1, 12);
+
+    play.getPlayer().setPlayerArr(1.1, 0);
+    play.getPlayer().setPlayerArr(1.2, 1);
+    play.getPlayer().setPlayerArr(1.3, 2);
+    play.getPlayer().setPlayerArr(3.1, 3);
+    play.getPlayer().setPlayerArr(3.2, 4);
+    play.getPlayer().setPlayerArr(3.3, 5);
 
     // test
     play.getComputer1().print();
