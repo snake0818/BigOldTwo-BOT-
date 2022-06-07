9241e21cc3de03229a1a2d60bbaf2ba8dc8d212b
GeloChiang <gelochiang2003@gmail.com>
2022-06-08 03:19:10 +0800

    開始正式啟用 mainForMac.cpp

--- BigTwo/mainForMac.cpp
+++ BigTwo/mainForMac.cpp
@@ -11,9 +11,9 @@
 
 int main()
 {
-    // Print().welcome();
-    // Print().rule();
-    // Print().shuffleCard();
+    Print().welcome();
+    Print().rule();
+    Print().shuffleCard();
 
     Table play;
     play.start();
@@ -21,76 +21,18 @@ int main()
 
     Print().printSequence(sequence);
 
-    Game().setField(3.1, 0);
-    Game().setField(3.3, 1);
-    Game().setField(3.4, 2);
-    Game().setField(0, 3);
-    Game().setField(0, 4);
-    Table().setCardsType(693);
+    while(!play.end())
+    {
+        play.getPlayer().printPlayer_arr();
 
-    // test
-    // play.getComputer1().setComputerArr(1.3, 0);
-    // play.getComputer1().setComputerArr(2.4, 1);
-    // play.getComputer1().setComputerArr(5.3, 2);
-    // play.getComputer1().setComputerArr(6.1, 3);
-    // play.getComputer1().setComputerArr(7.1, 4);
-    // play.getComputer1().setComputerArr(4.1, 5);
-    // play.getComputer1().setComputerArr(4.2, 6);
-    // play.getComputer1().setComputerArr(5.1, 7);
-    // play.getComputer1().setComputerArr(5.2, 8);
-    // play.getComputer1().setComputerArr(6.1, 9);
-    // play.getComputer1().setComputerArr(6.2, 10);
-    // play.getComputer1().setComputerArr(7.1, 11);
-    // play.getComputer1().setComputerArr(8.1, 12);
+        play.outCardSequence();
 
-    // play.getComputer2().setComputerArr(1.1, 0);
-    // play.getComputer2().setComputerArr(2.3, 1);
-    // play.getComputer2().setComputerArr(2.1, 2);
-    // play.getComputer2().setComputerArr(3.2, 3);
-    // play.getComputer2().setComputerArr(3.3, 4);
-    // play.getComputer2().setComputerArr(3.2, 5);
-    // play.getComputer2().setComputerArr(3.1, 6);
-    // play.getComputer2().setComputerArr(4.4, 7);
-    // play.getComputer2().setComputerArr(5.1, 8);
-    // play.getComputer2().setComputerArr(5.2, 9);
-    // play.getComputer2().setComputerArr(6.2, 10);
-    // play.getComputer2().setComputerArr(6.3, 11);
-    // play.getComputer2().setComputerArr(3.1, 12);
-
-    // play.getComputer3().setComputerArr(1.1, 0);
-    // play.getComputer3().setComputerArr(2.3, 1);
-    // play.getComputer3().setComputerArr(2.1, 2);
-    // play.getComputer3().setComputerArr(3.2, 3);
-    // play.getComputer3().setComputerArr(3.3, 4);
-    // play.getComputer3().setComputerArr(3.2, 5);
-    // play.getComputer3().setComputerArr(3.1, 6);
-    // play.getComputer3().setComputerArr(4.4, 7);
-    // play.getComputer3().setComputerArr(5.1, 8);
-    // play.getComputer3().setComputerArr(5.2, 9);
-    // play.getComputer3().setComputerArr(6.2, 10);
-    // play.getComputer3().setComputerArr(3.2, 11);
-    // play.getComputer3().setComputerArr(3.1, 12);
-
-    play.getPlayer().setPlayerArr(1.1, 0);
-    play.getPlayer().setPlayerArr(1.2, 1);
-    play.getPlayer().setPlayerArr(1.3, 2);
-    play.getPlayer().setPlayerArr(3.1, 3);
-    play.getPlayer().setPlayerArr(3.2, 4);
-    play.getPlayer().setPlayerArr(3.3, 5);
-
-    // test
-    play.getComputer1().print();
-    play.getComputer2().print();
-    play.getComputer3().print();
-    play.getPlayer().printPlayer_arr();
-
-    play.outCardSequence();
-
-    // test
-    play.getComputer1().print();
-    play.getComputer2().print();
-    play.getComputer3().print();
-    play.getPlayer().printPlayer_arr();
+        // // test
+        play.getComputer1().print();
+        play.getComputer2().print();
+        play.getComputer3().print();
+        play.getPlayer().printPlayer_arr();
+    }
 
     return 0;
 }
